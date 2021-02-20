7af5c7740e341b619d203533643012b6a5ebe64c
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-02-20 19:39:39 +0530

    add hello world api endpoint

--- src/main/java/com/csci5408/g17/FacilityBookingSystemApplication.java
+++ src/main/java/com/csci5408/g17/FacilityBookingSystemApplication.java
@@ -2,12 +2,28 @@ package com.csci5408.g17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class FacilityBookingSystemApplication {
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@RestController
+public class FacilityBookingSystemApplication{
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		SpringApplication.run(FacilityBookingSystemApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello(@RequestParam(value="name", required=false) String name){
+		String greeting;
+		if(name==null){
+			greeting = "Hello World!";
+		}
+		else{
+			greeting = String.format("Hello %s!", name);
+		}
+		return greeting;
+	}
 }
