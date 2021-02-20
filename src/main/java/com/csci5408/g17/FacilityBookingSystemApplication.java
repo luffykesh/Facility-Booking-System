4e1ba36b64efdac44a3777d511b99e1484f4b4d4
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-02-20 18:07:55 +0530

    init project

---
+++ src/main/java/com/csci5408/g17/FacilityBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.csci5408.g17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csci5408/g17/FacilityBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csci5408.g17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilityBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
