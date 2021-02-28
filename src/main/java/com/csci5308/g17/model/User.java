1728744b4dc54abfd8da34ab2392c29ba5654b71
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-02-28 11:40:29 +0530

    add user class

---
+++ src/main/java/com/csci5308/g17/model/User.java
@@ -0,0 +1,22 @@
+package com.csci5308.g17.model;
+
+public class User {
+    Integer id;
+    String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
