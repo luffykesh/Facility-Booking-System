8714cd913f0da16026bb04a48bc17bf8285fbed1
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-02-28 11:47:32 +0530

    add UserController

---
+++ src/main/java/com/csci5308/g17/controller/UserCountController.java
@@ -0,0 +1,23 @@
+package com.csci5308.g17.controller;
+
+import com.csci5308.g17.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserCountController {
+
+    UserService userService;
+
+    public UserCountController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/count")
+    public long getUserCount(){
+        return this.userService.getUserCount();
+    }
+}
