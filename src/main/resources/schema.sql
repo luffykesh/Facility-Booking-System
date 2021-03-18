59ee58b1648235fe824c1d268779155b8515aadd
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-03-19 01:07:55 +0530

    schema.sql: fix field lengths in user table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS user (
   verified tinyint DEFAULT '0',
   name text,
   email varchar(255) DEFAULT NULL,
-  password varchar(32) NOT NULL,
-  role varchar(45) DEFAULT NULL,
-  bannerId varchar(100) DEFAULT NULL,
+  password varchar(255) NOT NULL,
+  role varchar(255) DEFAULT NULL,
+  bannerId varchar(255) DEFAULT NULL,
 
   PRIMARY KEY (id)
 );
