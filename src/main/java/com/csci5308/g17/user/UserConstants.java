5aa125fad4be92bba3e5838665e2fd829d682ef9
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-03-20 20:37:25 +0530

    add user role constants

---
+++ src/main/java/com/csci5308/g17/user/UserConstants.java
@@ -0,0 +1,7 @@
+package com.csci5308.g17.user;
+
+public class UserConstants {
+    public static final String USER_ROLE_ADMIN = "ADMIN";
+    public static final String USER_ROLE_MANAGER = "MANAGER";
+    public static final String USER_ROLE_USER = "USER";
+}
