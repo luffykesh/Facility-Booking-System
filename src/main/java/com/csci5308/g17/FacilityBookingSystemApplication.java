92806412e07eefe31aaa1eb9cb0c05cae8b402a6
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-03-20 21:29:21 +0530

    add login path

--- src/main/java/com/csci5308/g17/FacilityBookingSystemApplication.java
+++ src/main/java/com/csci5308/g17/FacilityBookingSystemApplication.java
@@ -3,16 +3,19 @@ package com.csci5308.g17;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
-@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
+@SpringBootApplication
 @Controller
 public class FacilityBookingSystemApplication{
 
 	public static void main(String[] args){
 		SpringApplication.run(FacilityBookingSystemApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String login(){
+		return "login";
+	}
 }
---
+++ src/main/java/com/csci5308/g17/MainController.java
@@ -0,0 +1,35 @@
+package com.csci5308.g17;
+
+import java.security.Principal;
+
+import com.csci5308.g17.user.User;
+import com.csci5308.g17.user.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/admin/home")
+    public ModelAndView adminHome(Principal principal) {
+        // Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        // Principal loggedUser =
+        System.out.println("\n\nQuerying user by: " + principal.getName());
+        User u = userService.getUserByEmail(principal.getName());
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("admin");
+        mav.addObject("user", u);
+        return mav;
+    }
+}
