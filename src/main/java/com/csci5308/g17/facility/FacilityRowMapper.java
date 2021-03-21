c80d5f4344c67488513682cfe46aaaebfc174451
Vivekkumar Rasikbhai Sanepara <vv219484@dal.ca>
2021-03-21 15:14:13 +0530

    Modified Row Mapper for Facility class

--- src/main/java/com/csci5308/g17/facility/FacilityRowMapper.java
+++ src/main/java/com/csci5308/g17/facility/FacilityRowMapper.java
@@ -1,4 +1,23 @@
 package com.csci5308.g17.facility;
 
-public class FacilityRowMapper {
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class FacilityRowMapper implements RowMapper<Facility> {
+    @Override
+    public Facility mapRow(ResultSet rs, int rowNumber) throws SQLException {
+        Facility facility = new Facility();
+        facility.setId(rs.getInt("id"));
+        facility.setName(rs.getString("name"));
+        facility.setDescription(rs.getString("description"));
+        facility.setLocation(rs.getString("location"));
+        facility.setOccupancy(rs.getInt("occupancy"));
+        facility.setManager_id(rs.getInt("manager_id"));
+        facility.setTime_slot(rs.getString("time_slot"));
+        facility.setActive(rs.getBoolean("active"));
+        facility.setApproval_required(rs.getBoolean("approval_required"));
+        return facility;
+    }
 }
