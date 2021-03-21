581f6733c5e78807596298059e3a96673e0853c7
Vivekkumar Rasikbhai Sanepara <vv219484@dal.ca>
2021-03-21 15:10:12 +0530

    changes in getFacilitybyId return type and added findAll

--- src/main/java/com/csci5308/g17/facility/FacilityController.java
+++ src/main/java/com/csci5308/g17/facility/FacilityController.java
@@ -1,6 +1,5 @@
 package com.csci5308.g17.facility;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,18 +9,24 @@ import java.util.List;
 public class FacilityController {
 
     IFacilityService facilityService;
+
     public FacilityController(IFacilityService facilityService) {
         this.facilityService = facilityService;
     }
 
     @GetMapping(value = "/{id}")
-    public List<Facility> getFacilityById(@PathVariable(value="id") int id){
+    public Facility getFacilityById(@PathVariable(value = "id") int id) {
         System.out.println(id);
         return this.facilityService.getFacilityById(id);
     }
 
     @PostMapping()
-    public void Save(@ModelAttribute("facility") Facility facility){
-        this.facilityService.Save(facility);
+    public void Save(@ModelAttribute("facility") FormFacility formFacility) {
+        this.facilityService.Save(formFacility);
+    }
+
+    @GetMapping()
+    public List<Facility> findAll() {
+        return this.facilityService.findAll();
     }
 }
