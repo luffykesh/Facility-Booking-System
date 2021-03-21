24640cacce59d91e0d557fcf5a6123898d93141b
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-03-21 21:08:15 +0530

    fix app crash

--- src/main/java/com/csci5308/g17/MainController.java
+++ src/main/java/com/csci5308/g17/MainController.java
@@ -2,10 +2,10 @@ package com.csci5308.g17;
 
 import java.security.Principal;
 
-import com.csci5308.g17.user.IUserService;
 import com.csci5308.g17.user.User;
 import com.csci5308.g17.user.UserService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,15 +13,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MainController {
 
-    IUserService userService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/login")
     public String loginPage() {
         return "login";
-    }
-
-    public MainController() {
-        userService = UserService.getInstance();
     }
 
     @GetMapping("/admin/home")
