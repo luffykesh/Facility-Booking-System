76cc454ab040a82a84ae8f5efeb729ea84332b35
Helly Patel <hl737563@dal.ca>
2021-03-22 13:16:35 +0530

    Added interface for ResetPasswordService

---
+++ src/main/java/com/csci5308/g17/user/IResetPasswordService.java
@@ -0,0 +1,7 @@
+package com.csci5308.g17.user;
+
+public interface IResetPasswordService {
+    User check_email(String mailid, int token) throws UserNotFoundException;
+    User check_token(int token);
+    Integer updatePassword(int token, String password);
+}
