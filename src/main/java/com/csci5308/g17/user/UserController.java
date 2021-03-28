6f7f583c2a289f505f58d0a45b3506fc8546ce90
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-03-28 17:53:03 +0530

    merge CSVcontroller into UserController

--- src/main/java/com/csci5308/g17/user/UserController.java
+++ src/main/java/com/csci5308/g17/user/UserController.java
@@ -1,21 +1,35 @@
 package com.csci5308.g17.user;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController
-@RequestMapping("/user")
+import java.io.IOException;
+import java.util.List;
+@Controller
 public class UserController {
 
     IUserService userService;
+    private ICSVservice csvService;
 
-    public UserController() {
+    public UserController(CSVservice csvService) {
         userService = UserService.getInstance();
+        this.csvService = csvService;
     }
 
-    @GetMapping("/count")
-    public long getUserCount() {
-        return userService.getUserCount();
+    @GetMapping("/admin/user/upload")
+    public String userCSVUploadForm(){
+        return "upload";
+    }
+
+    @PostMapping("/admin/user/upload")
+    public String userCSVData(@RequestParam(name="file") MultipartFile file) throws IOException {
+        if(csvService.hasCSVFormat(file)) {
+            List<User> userList = csvService.readCSV(file.getInputStream());
+            userService.savetoDB(userList);
+        }
+        return "redirect:/admin/home";
     }
 }
