dbacf9821c179cac5a5562de639c322185133df7
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-03-29 13:15:39 +0530

    format fixes

--- src/main/java/com/csci5308/g17/user/IUserRepository.java
+++ src/main/java/com/csci5308/g17/user/IUserRepository.java
@@ -2,11 +2,19 @@ package com.csci5308.g17.user;
 
 public interface IUserRepository {
     User getUserById(Integer Id);
+
     User getUserByEmail(String email);
+
     Integer count();
+
     User getUserByToken(String token);
+
     void save(User u);
+
     void setUserToken(String email, String token);
+
     void updatePassword(Integer id, String password);
+
     void clearUserToken(Integer userId);
+
 };
