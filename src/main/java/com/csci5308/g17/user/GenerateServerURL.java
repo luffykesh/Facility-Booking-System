594437aa3df0a45f8cc95983e78cd6c26d7a0f26
Helly Patel <hl737563@dal.ca>
2021-03-31 00:33:39 +0530

    Created different class to generate URL

--- src/main/java/com/csci5308/g17/user/GenerateServerURL.java
+++ src/main/java/com/csci5308/g17/user/GenerateServerURL.java
@@ -1,2 +1,16 @@
-package com.csci5308.g17.user;public class GenerateServerURL {
+package com.csci5308.g17.user;
+
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+@Service
+public class GenerateServerURL implements IGenerateServerURL {
+    @Override
+    public String getURL(HttpServletRequest request){
+        String serverUrl = String.format(
+                "%s://%s:%d",
+                request.getScheme(), request.getServerName(), request.getServerPort());
+        return serverUrl;
+
+    }
 }
