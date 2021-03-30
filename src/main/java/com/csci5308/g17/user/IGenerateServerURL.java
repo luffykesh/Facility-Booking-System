389ce2c6b1b01a0186d04b58e7d2e079c6d25d0c
Helly Patel <hl737563@dal.ca>
2021-03-31 00:35:04 +0530

    interface for generate URL class

--- src/main/java/com/csci5308/g17/user/IGenerateServerURL.java
+++ src/main/java/com/csci5308/g17/user/IGenerateServerURL.java
@@ -1,2 +1,7 @@
-package com.csci5308.g17.user;public interface IGenerateServerURL {
+package com.csci5308.g17.user;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface IGenerateServerURL {
+    String getURL(HttpServletRequest request);
 }
