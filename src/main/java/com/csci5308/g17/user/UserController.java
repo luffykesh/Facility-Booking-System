9cdd99e3ef4c39a24669a740a5ea121ebc62882d
Helly Patel <hl737563@dal.ca>
2021-04-02 21:10:26 +0530

    Did changes in email methods

--- src/main/java/com/csci5308/g17/user/UserController.java
+++ src/main/java/com/csci5308/g17/user/UserController.java
@@ -3,6 +3,7 @@ package com.csci5308.g17.user;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,33 +20,35 @@ public class UserController {
     private IUserCSVParser csvService;
     private IEmailService emailService;
     private IVerifyUser verifyUser;
-    private List<String> emailList;
-    private IGenerateServerURL url;
+    private UserConstants userConstants;
 
-
-
-    public UserController(UserCSVParser csvService, EmailService emailService, VerifyUser varifyUser, List emailList, GenerateServerURL url) {
+    public UserController(UserCSVParser csvService, EmailService emailService, VerifyUser varifyUser, List emailList,UserConstants userConstants) {
         userService = UserService.getInstance();
         this.csvService = csvService;
         this.emailService = emailService;
         this.verifyUser=varifyUser;
-        this.emailList=emailList;
-        this.url=url;
-
-
+        this.userConstants=userConstants;
     }
 
-    public void sendEmail(String email,HttpServletRequest request,String content,String link){
-        String serverUrl=url.getURL(request);
+    private void addAndEmailUser(String email,HttpServletRequest request,String content,String link){
+        String serverUrl=getURL(request);
         try {
             String token = userService.setUserToken(email);
-            String resetPasswordLink = String.format("%s/"+link+"/%s", serverUrl, token);
-            emailService.sendEmail(email, token, resetPasswordLink,content);
+            String resetPasswordLink = String.format("%s/%s/%s", serverUrl,link,token);
+            String emailContent=String.format(content, resetPasswordLink);
+            emailService.sendEmail(email,emailContent);
         } catch (UserNotFoundException e) {
             e.printStackTrace();
         } catch (MessagingException e) {
             e.printStackTrace();
         }
+    }
+
+    private String getURL(HttpServletRequest request){
+        String serverUrl = String.format(
+                "%s://%s:%d",
+                request.getScheme(), request.getServerName(), request.getServerPort());
+        return serverUrl;
     }
 
     @GetMapping("/admin/user_upload")
@@ -58,45 +61,43 @@ public class UserController {
         if(csvService.isCSVFormat(file)) {
             List<User> userList = csvService.readCSV(file.getInputStream());
             userService.addAll(userList);
-            String link="Varification_Form";
+            String link="verification_form";
             String content="<p>Hello,</p>"
                     + "<p>Click the link below to verify your account:</p>"
                     + "<p><a href=\"%s\">Account Verification</a></p>";
-
-            emailList=verifyUser.getUserList(userList);
-            for(int i=0;i<emailList.size();i++) {
-                sendEmail(emailList.get(i),request,content,link);
+            List<String> userEmails = userList.stream().map((user) -> user.getEmail()).collect(Collectors.toList());
+            for(int i=0;i<userEmails.size();i++) {
+                addAndEmailUser(userEmails.get(i),request,content,link);
             }
-
         }
         return "redirect:/admin/home";
     }
 
-    @GetMapping("/Varification_Form/{token}")
+    @GetMapping("/verification_form/{token}")
     public String getVerificationForm(@PathVariable("token") String token, Model model) {
         User user=userService.getUserByToken(token);
         if (user == null) {
             model.addAttribute("message", "Invalid Token");
         }
-        return "Varification_Form";
+        return "verification_form";
     }
-    @PostMapping("/Varification_Form")
+
+    @PostMapping("/verification_form")
     public String verifyUser(@RequestParam(value="token") String token, @RequestParam(value = "password") String password, Model model){
         User user=userService.getUserByToken(token);
 
         if (user == null) {
             model.addAttribute("message", "Invalid Token");
-            return "Varification_Form";
+            return "verification_form";
         }
         else {
-            int verified=1;
+
             userService.updatePassword(user.getId(), password);
-            userService.setVerification(user.getId(),verified);
+            userService.verifyUser(user.getId());
             userService.clearUserToken(user.getId());
             model.addAttribute("message", "Verified");
         }
-        return "Varification_Form";
-
+        return "verification_form";
     }
 
     @GetMapping("/forgot_password")
@@ -104,16 +105,14 @@ public class UserController {
         return "Forgot_Password_Form";
     }
 
-
-
     @PostMapping("/forgot_password")
     public String processPassword(@RequestParam(name="email") String email, Model model,HttpServletRequest request) {
-        String serverUrl=url.getURL(request);
+        String serverUrl=getURL(request);
         String link="reset_password";
         String content="<p>Hello,</p>"
                 + "<p>Click the link below to reset your password:</p>"
-                + "<p><a href=\"%s\">Reset Passworf</a></p>";
-        sendEmail(email,request,content,link);
+                + "<p><a href=\"%s\">Reset Password</a></p>";
+        addAndEmailUser(email,request,content,link);
 
         model.addAttribute("message", "Password reset link has been sent to your email");
         return "Forgot_Password_Form";
@@ -144,28 +143,27 @@ public class UserController {
         return "Reset_Password_Form";
     }
 
-    @GetMapping("/User_Regestration")
+    @GetMapping("/user_regestration_form")
     public String getRegestrationForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
 
-        List<String> listRoles = Arrays.asList("Manager", "User");
+        List<String> listRoles = Arrays.asList(userConstants.USER_ROLE_MANAGER, userConstants.USER_ROLE_USER);
         model.addAttribute("listRoles", listRoles);
 
-        return "/User_Regestration";
+        return "user_regestration_form";
     }
 
-    @PostMapping("/User_Regestration")
+    @PostMapping("/user_regestration_form")
     public String processForm(@ModelAttribute("user") User user,Model model,HttpServletRequest request) {
         userService.save(user);
-        String link="Varification_Form";
+        String link="verification_form";
         String content="<p>Hello,</p>"
                 + "<p>Click the link below to verify your account:</p>"
-                + "<p><a href=\"%s\">Varify Account</a></p>";
-        sendEmail(user.getEmail(),request,content,link);
+                + "<p><a href=\"%s\">Verify Account</a></p>";
+        addAndEmailUser(user.getEmail(),request,content,link);
         model.addAttribute("message", "User added successfully");
-        return "/User_Regestration";
-
+        return "user_regestration_form";
     }
 
 }
