af7b34a7ac5d019b220f7ae3c5f700b3f75605b3
Vivekkumar Rasikbhai Sanepara <vv219484@dal.ca>
2021-04-06 13:51:58 +0530

    minor changes

--- src/main/java/com/csci5308/g17/timing/TimingController.java
+++ src/main/java/com/csci5308/g17/timing/TimingController.java
@@ -1,30 +1,21 @@
 package com.csci5308.g17.timing;
 
+import com.csci5308.g17.auth.CurrentUserService;
+import com.csci5308.g17.utils.JsonResponseDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import com.csci5308.g17.auth.CurrentUserService;
-import com.csci5308.g17.utils.JsonResponseDTO;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/timing")
@@ -40,7 +31,7 @@ public class TimingController {
     }
 
     @GetMapping("")
-    public String getTimingsForFacility(@RequestParam(name="facility_id") Integer facilityId, Model model) {
+    public String getTimingsForFacility(@RequestParam(name = "facility_id") Integer facilityId, Model model) {
         List<Timing> timings = timingService.getFacilityTimings(facilityId);
         JsonResponseDTO<List<TimingDTO>> responseData = null;
         ResponseEntity<JsonResponseDTO> response = null;
@@ -49,26 +40,21 @@ public class TimingController {
             List<TimingDTO> timingDTOs = timings.stream().map((timing) -> new TimingDTO(timing)).collect(Collectors.toList());
             responseData = new JsonResponseDTO(true, "Timing list", null, timingDTOs);
             response = new ResponseEntity(responseData, HttpStatus.OK);
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             String errMessage = "Error retriving timings from database";
             logger.error(errMessage, e);
             responseData = new JsonResponseDTO(false, null, errMessage, null);
             response = new ResponseEntity(responseData, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        model.addAttribute("facilityId",facilityId);
-        model.addAttribute("user",currentUserService.getCurrentUser());
-        model.addAttribute("timingList",timings);
+        model.addAttribute("facilityId", facilityId);
+        model.addAttribute("user", currentUserService.getCurrentUser());
+        model.addAttribute("timingList", timings);
         return "timing";
     }
-
-    //@PostMapping(path = "", consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})  ResponseEntity<JsonResponseDTO>
+    
     @PostMapping()
     public RedirectView addTiming(@ModelAttribute("timing") TimingDTO requestTiming, RedirectAttributes attributes) {
-        System.out.println(requestTiming.getStartTime());
-        System.out.println(requestTiming.getEndTime());
-        System.out.println(requestTiming.getDay());
-        System.out.println(requestTiming.getFacilityId());
+
         JsonResponseDTO<String> responseData = null;
         ResponseEntity<JsonResponseDTO> response = null;
         Timing newTiming = new Timing();
@@ -82,38 +68,39 @@ public class TimingController {
             newTiming = timingService.addTiming(newTiming);
             responseData = new JsonResponseDTO(true, "Timing added", null, newTiming);
             response = new ResponseEntity(responseData, HttpStatus.OK);
-        }
-        catch(TimingConflictException e) {
+        } catch (TimingConflictException e) {
             logger.error(e.getT1() + " conflict with " + e.getT2(), e);
             responseData = new JsonResponseDTO(false, "Cannot add conflicting timing", "Timing conflict", null);
             response = new ResponseEntity(responseData, HttpStatus.CONFLICT);
-        }
-        catch(IllegalArgumentException e) {
+        } catch (IllegalArgumentException e) {
             logger.error("Invalid time range", e);
             responseData = new JsonResponseDTO(false, e.getMessage(), "Invalid time range", null);
             response = new ResponseEntity(responseData, HttpStatus.BAD_REQUEST);
         }
-        attributes.addAttribute("facility_id",requestTiming.getFacilityId());
+        attributes.addAttribute("facility_id", requestTiming.getFacilityId());
+        attributes.addFlashAttribute("message", responseData.getMessage());
         return new RedirectView("/timing");
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<JsonResponseDTO> deleteTiming(@PathVariable("id") Integer timingId) {
+    @GetMapping("/{id}")
+    public RedirectView deleteTiming(@PathVariable("id") Integer timingId, RedirectAttributes attributes) {
         JsonResponseDTO<String> responseData = null;
         ResponseEntity<JsonResponseDTO> response = null;
-
-        try{
+        Timing timing = timingService.getTiming(timingId);
+        try {
             timingService.deleteTiming(timingId);
-            logger.info("Deleted timing: "+ timingId);
+            logger.info("Deleted timing: " + timingId);
             responseData = new JsonResponseDTO(true, "Timing deleted", null, null);
             response = new ResponseEntity(responseData, HttpStatus.OK);
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             logger.error("Error deleting exception", e);
             responseData = new JsonResponseDTO(true, "Error deleting", null, null);
             response = new ResponseEntity(responseData, HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return response;
+
+        attributes.addAttribute("facility_id", timing.getFacilityId());
+        attributes.addFlashAttribute("message", responseData.getMessage());
+        return new RedirectView("/timing");
     }
 
 }
