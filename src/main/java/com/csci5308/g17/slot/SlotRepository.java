ccd0ad2f6c6071c0041786c8994c9fd1d0982b85
Rushikesh Patel <rushikesh.patel@dal.ca>
2021-04-07 03:35:09 +0530

    fix reserve slot and release slot query

--- src/main/java/com/csci5308/g17/slot/SlotRepository.java
+++ src/main/java/com/csci5308/g17/slot/SlotRepository.java
@@ -22,8 +22,8 @@ public class SlotRepository implements ISlotRepository{
     private final String Q_SLOTS_OF_TIMING = "SELECT * FROM slot where timing_id = ? ORDER BY start_time";
     private final String Q_INSERT_SLOT = "INSERT INTO slot(facility_id, timing_id, total_seats, available_seats, start_time, end_time) VALUES(?,?,?,?,?,?)";
     private final String Q_DELETE_SLOTS_OF_TIMING = "DELETE FROM slot WHERE timing_id = ?";
-    private final String Q_RESERVE_SEAT = "UPDATE slot SET available_seats=available_seats-1 where id=?";
-    private final String Q_RELEASE_SEAT = "UPDATE slot SET available_seats=available_seats+1 where id=?";
+    private final String Q_RESERVE_SEAT = "UPDATE slot SET available_seats=available_seats-1 where id=? and available_seats>0";
+    private final String Q_RELEASE_SEAT = "UPDATE slot SET available_seats=available_seats+1 where id=? and available_seats<total_seats";
     private final String Q_SLOT_BY_ID = "SELECT * FROM slot where id = ?";
 
     public SlotRepository(JdbcTemplate jdbcTemplate) {
